23b3f93b68b0fd1f4f29255820e5f0eb1d6ce214
eNDO9 <ndo@isdglobal.org>
2024-10-19 16:45:59 -0400

    test

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -1,35 +1,11 @@
-import numpy as np
-import altair as alt
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.header('st.write')
+st.header('Line chart')
 
-# Example 1
-
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(34987507923457093247509823978045)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+
+st.line_chart(chart_data)
