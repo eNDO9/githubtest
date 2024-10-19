e6288f49bc507471b619ce0cdece18a855c6aa13
eNDO9 <ndo@isdglobal.org>
2024-10-19 16:53:54 -0400

    t

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-st.header('st.checkbox')
+st.header('st.multiselect')
 
-st.write ('What would you like to order?')
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
 
-icecream = st.checkbox('Ice cream')
-coffee = st.checkbox('Coffee')
-cola = st.checkbox('Cola')
-
-if icecream:
-     st.write("Great! Here's some more 🍦")
-
-if coffee: 
-     st.write("Okay, here's some coffee ☕")
-
-if cola:
-     st.write("Here you go 🥤")
+if options:
+    st.write('You selected:', options)
