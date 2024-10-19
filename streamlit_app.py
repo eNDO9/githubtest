e300d6f2f183a22547929da17bee4d10b1398ddd
eNDO9 <ndo@isdglobal.org>
2024-10-19 17:56:37 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
-import ydata_profiling as pandas_profiling
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 st.header('`streamlit_pandas_profiling`')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-pr = df.profile_report()
+pr = ProfileReport(df)
 st_profile_report(pr)
